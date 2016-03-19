70d187a65943b4a3de45e7ef241a508feffcdbb2
Richard <orsusbass@gmail.com>
2016-03-19 17:54:33 -0500

    Add specs and code for editing todo items

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -65,4 +65,10 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
+end
+
+RSpec.configure do |c|
+  # RSpec::Core::ExampleGroup#example is deprecated and will be removed in RSpec 3.
+  # [...] snippet to continue making this method available in RSpec 2.99 and RSpec 3:
+  c.expose_current_running_example_as :example
 end
